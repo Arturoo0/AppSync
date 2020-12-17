82e8b8d94097b6f9f2ad1c5b49f90e303b1130d6
Arturoo0 <arturoportelles@gmail.com>
2020-12-17 00:23:56 -0500

    log out post req

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const resGen = require('../Utils/responseGenerator.js'); 
 
 router.post('/login', (req, res) => {
+    console.log(req);
     res.send(resGen.generate200({authRouteType : 'login'}));
 });
 
 router.post('/signup', (req, res) => {
+    console.log(req);
     res.send(resGen.generate200({authRouteType : 'signup'}));
 });
