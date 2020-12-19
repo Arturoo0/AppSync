61cb9d801385713961b71a2d193b2744b8cf335a
Arturoo0 <arturoportelles@gmail.com>
2020-12-18 19:52:20 -0500

    workingdata pipeline to server

--- Frontend/src/utils/baseRequest.js
+++ Frontend/src/utils/baseRequest.js
@@ -1,15 +1,12 @@
 
 const axios = require('axios');
 
-export const postUserInfo = async (userInfo) => {
-    await axios.post('https://localhost:8000/auth/signup',
+export const postUserInfo = (userInfo) => {
+    axios.post('http://localhost:8000/auth/signup',
         userInfo
     )
     .then((response) => {
         console.log(response);
-    })
-    .catch((error) => {
-        console.log('Erorr on post.');
     })
 }
 
